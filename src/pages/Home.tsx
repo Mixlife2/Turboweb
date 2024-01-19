46462089a997cab85e099b5978b7d4868f1aad8c
Eddy villalba <eddykratochvil@gmail.com>
2024-01-18 21:56:32 -0300

    migratepagev2

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Inicio</h1>
+      {/* Contenido de la página principal */}
+    </div>
+  );
+}
+
+export default Home;
